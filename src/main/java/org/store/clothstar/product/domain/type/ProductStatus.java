7d447f4f827dfc93abe986d600452515cd44eb47
Ogu1208 <kdasunb@naver.com>
2024-03-23 07:55:30 +0900

    refactor: ProductStatus ENUM에 description 필드 제거
    영어로만 관리 하는 것이 좋음. (어차피 프론트와 enum값 협의)

--- src/main/java/org/store/clothstar/product/domain/type/ProductStatus.java
+++ src/main/java/org/store/clothstar/product/domain/type/ProductStatus.java
@@ -4,12 +4,10 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum ProductStatus {
-    COMING_SOON("준비중"),
-    FOR_SALE("판매중"),
-    ON_SALE("할인중"),
-    SOLD_OUT("품절"),
-    HIDDEN("숨김"),
-    DISCOUNTED("단종");
-    private final String description;
-
+    COMING_SOON,
+    FOR_SALE,
+    ON_SALE,
+    SOLD_OUT,
+    HIDDEN,
+    DISCOUNTED;
 }
