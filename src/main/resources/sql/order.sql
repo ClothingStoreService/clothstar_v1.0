1810cfabe2dcea02c6f78fa6a8f8afbba20298a8
subin <axhtl@gachon.ac.kr>
2024-03-23 10:19:19 +0900

    refactor: 테이블 이름 변경 orderline -> orders 및 컬럼 이름 및 타입 변경
    - 컬럼 이름 변경
    'created_dt' -> 'created_date'
    - 컬럼 타입 변경
    created_date 타입: varchar(255) -> timestamp
    created_at 타입: datetime -> timestamp

--- src/main/resources/sql/order.sql
+++ src/main/resources/sql/order.sql
@@ -1,29 +1,36 @@
-DROP TABLE IF EXISTS `orderline`;
+DROP TABLE IF EXISTS orders;
 
-CREATE TABLE `orderline`
+CREATE TABLE orders
 (
     `order_id`       bigint       NOT NULL,
     `member_id`      bigint       NOT NULL,
-    `delivery_id`    BIGINT       NOT NULL,
-    `created_dt`     varchar(255) NOT NULL,
-    `created_at`     datetime     NOT NULL DEFAULT now(),
-    `status`         varchar(255) NOT NULL DEFAULT 'approve',
+    `delivery_id`    bigint       NOT NULL,
+    `created_date`   timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `created_at`     timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `status`         varchar(255) NOT NULL DEFAULT 'APPROVE',
     `shipping_amt`   int          NOT NULL,
     `products_amt`   int          NOT NULL,
     `payment_method` varchar(255) NOT NULL,
     `payment_amt`    int          NOT NULL
 );
 
-ALTER TABLE `orderline`
-    ADD CONSTRAINT `PK_ORDERLINE` PRIMARY KEY (
-                                               `order_id`
+ALTER TABLE orders
+    ADD CONSTRAINT `PK_ORDER` PRIMARY KEY (
+                                           `order_id`
         );
 
+ALTER TABLE orders
+    DROP PRIMARY KEY;
+
 SELECT *
-FROM orderline;
+FROM orders;
 
 
-INSERT INTO orderline (order_id, member_id, delivery_id, created_dt, created_at, status, shipping_amt, products_amt,
-                       payment_method, payment_amt)
-VALUES ('1', '2', '3', 'a', now(), 'b', '4', '5', '6', '7');
+INSERT INTO orders (order_id, member_id, delivery_id, created_date, created_at, status, shipping_amt, products_amt,
+                    payment_method, payment_amt)
+VALUES ('14241232', '242', '334', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'APPROVE', '3000', '50000', 'CARD', '53000');
 
+INSERT INTO orders (order_id, member_id, delivery_id, shipping_amt,
+                    products_amt,
+                    payment_method, payment_amt)
+VALUES ('142412321', '242', '334', '3000', '50000', 'CARD', '53000');
