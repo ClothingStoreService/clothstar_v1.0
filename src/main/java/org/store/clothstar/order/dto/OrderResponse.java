87b4124509e8e99d429bf6920d7c4fa9161185bd
subin <axhtl@gachon.ac.kr>
2024-03-23 11:58:19 +0900

    style: OrderResponse에 빌더 어노테이션 주석 삭제

--- src/main/java/org/store/clothstar/order/dto/OrderResponse.java
+++ src/main/java/org/store/clothstar/order/dto/OrderResponse.java
@@ -20,29 +20,4 @@ public class OrderResponse {
 	private int totalProductsPrice;
 	private PaymentMethod paymentMethod;
 	private int totalPrice;
-
-	// @Builder
-	// public OrderResponse(
-	// 	Long orderId,
-	// 	Long memberId,
-	// 	Long deliveryId,
-	// 	LocalDateTime createdDate,
-	// 	LocalDateTime createdTime,
-	// 	String status,
-	// 	int totalShippingPrice,
-	// 	int totalProductsPrice,
-	// 	PaymentMethod paymentMethod,
-	// 	int totalPrice
-	// ) {
-	// 	this.orderId = orderId;
-	// 	this.memberId = memberId;
-	// 	this.deliveryId = deliveryId;
-	// 	this.createdDate = createdDate;
-	// 	this.createdTime = createdTime;
-	// 	this.status = status;
-	// 	this.totalShippingPrice = totalShippingPrice;
-	// 	this.totalProductsPrice = totalProductsPrice;
-	// 	this.paymentMethod = paymentMethod;
-	// 	this.totalPrice = totalPrice;
-	// }
 }
